b7a91e9e3ad961646eca541558c393f4eb6d8200
Dan Heitbohmer <dan@conceptual.ca>
2023-04-11 16:09:48 +0000

    Dummy Data Inserted

--- projects/webflow-ng-availability-calendar/src/components/availability-calendar/availability-calendar.component.ts
+++ projects/webflow-ng-availability-calendar/src/components/availability-calendar/availability-calendar.component.ts
@@ -1,12 +1,23 @@
-import { ChangeDetectionStrategy, Component, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatDatepickerModule } from '@angular/material/datepicker';
+import { AvailabilityService } from './data/availability.service';
+import { MatNativeDateModule } from '@angular/material/core';
 @Component({
   standalone: true,
-  imports: [CommonModule, MatCardModule, MatDatepickerModule],
+  imports: [
+    CommonModule,
+    MatCardModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+  ],
   templateUrl: './availability-calendar.component.html',
   styleUrls: ['./availability-calendar.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AvailabilityCalendarComponent {}
+export class AvailabilityCalendarComponent {
+  protected readonly availability$ = this.availabilityService.availability$;
+
+  constructor(private readonly availabilityService: AvailabilityService) {}
+}
