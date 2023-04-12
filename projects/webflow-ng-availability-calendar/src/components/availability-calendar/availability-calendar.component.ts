fbd5f5091e9d4a9254ffbf325620d074d41eb549
Dan Heitbohmer <dan@conceptual.ca>
2023-04-12 14:36:48 +0000

    broken

--- projects/webflow-ng-availability-calendar/src/components/availability-calendar/availability-calendar.component.ts
+++ projects/webflow-ng-availability-calendar/src/components/availability-calendar/availability-calendar.component.ts
@@ -4,6 +4,8 @@ import { MatCardModule } from '@angular/material/card';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { AvailabilityService } from './data/availability.service';
 import { MatNativeDateModule } from '@angular/material/core';
+import { FormControl, FormGroup } from '@angular/forms';
+
 @Component({
   standalone: true,
   imports: [
@@ -20,4 +22,10 @@ export class AvailabilityCalendarComponent {
   protected readonly availability$ = this.availabilityService.availability$;
 
   constructor(private readonly availabilityService: AvailabilityService) {}
+
+  //calculate this month and set as start to future rolling 6 months
+
+  //calculate the future 6 months
+
+  //get the source data and style those dates as available
 }
