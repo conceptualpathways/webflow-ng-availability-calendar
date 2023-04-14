e13f31e3d77f370e7d937777f0a4391671ea72f8
Dan Erdelt <erd@conceptual.ca>
2023-04-14 19:55:51 +0000

    cleanup

--- projects/webflow-ng-availability-calendar/src/components/availability-calendar/data/availability.service.ts
+++ projects/webflow-ng-availability-calendar/src/components/availability-calendar/data/availability.service.ts
@@ -6,12 +6,12 @@ import { AvailabilityModel } from './availability-model';
   providedIn: 'root',
 })
 export class AvailabilityService {
-  public readonly availability$: Observable<AvailabilityModel[]> = of(document.querySelectorAll('script[type="application/json"].calendar-data')).pipe(
-    map(elms =>
-      Array.from(elms)
-        .map(elm =>
-          JSON.parse(elm.textContent || 'undefined') as AvailabilityModel
-        ).filter(item => !!item)
+  public readonly availability$: Observable<AvailabilityModel[]> = of(
+    document.querySelectorAll('script[type="application/json"].calendar-data')
+  ).pipe(
+    map(elms => Array.from(elms)
+      .map(elm => JSON.parse(elm.textContent || 'undefined') as AvailabilityModel)
+      .filter(item => !!item)
     )
   );
 
